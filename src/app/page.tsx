9dba4b259edd43b2ffd30e3902b40761b2d79468
antonio blanquiceth <anto0jose9@gmail.com>
2025-09-07 00:14:36 +0000

    Try fixing this error: `Runtime Error: Error: useAppContext must be used

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,11 +1,14 @@
 'use client';
 
 import { Dashboard } from '@/components/agendu/Dashboard';
+import { AppProvider } from '@/contexts/app-provider';
 
 export default function Home() {
   return (
     <main className="container mx-auto p-4 sm:p-6 lg:p-8">
-      <Dashboard />
+      <AppProvider>
+        <Dashboard />
+      </AppProvider>
     </main>
   );
 }
